c1f09a98ba33b312a89bcaa5bc3a3319a7815a49
Kundan <100273989+InfiniteCoder100@users.noreply.github.com>
2023-08-20 16:19:58 +0530

    Create variables.go

---
+++ variables.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+  //Ways of declaring variables
+	var a string = "apple"      //method-1
+	var b = 10.7               //method-2  
+	c := true                  //method-3
+	var d float32              //method-4
+	fmt.Printf("the value of a :%v and type is %T \n", a, a)
+	fmt.Printf("the value of b:%v and type is %T \n", b, b)
+	fmt.Printf("the value of c :%v and type is %T\n", c, c)
+	fmt.Printf("the value of d :%v and type is %T", d, d)
+
+}
